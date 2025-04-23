e5c2d62af2b4d2ef2416f4c7183890a399102af9
lidyaocto <octo.lidya@gmail.com>
2025-04-23 07:07:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function tampil() {
+  document.getElementById("Alert").style.display = "block";
+  document.getElementById("Overlay").style.display = "block";
+}
+function tutup() {
+  document.getElementById("Alert").style.display = "none";
+  document.getElementById("Overlay").style.display = "none";
+}
